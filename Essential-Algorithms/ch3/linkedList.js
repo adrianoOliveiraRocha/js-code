95222430750d2748258068a573d1cd37432168ca
w3software-br <w3software.br@gmail.com>
2021-09-02 08:09:02 -0300

    #

--- Essential-Algorithms/ch3/linkedList.js
+++ Essential-Algorithms/ch3/linkedList.js
@@ -10,8 +10,8 @@ let ic47 = new IntegerCell(); ic47.value = 47; ic47.next = ic9;
 let ic72 = new IntegerCell(); ic72.value = 72; ic72.next = ic47;
 let ic31 = new IntegerCell(); ic31.value = 31; ic31.next = ic72;
 
-function Iterate(top) {
-  while(top) {
+function Iterate(top) { // O(N)
+  while(top) { // O(N)
     console.log(top.value);
     top = top.next;
   }
@@ -19,11 +19,3 @@ function Iterate(top) {
 
 Iterate(ic31);
 
-function findCell(top, target) {
-  while(top) {
-    if(top.value === target) return top;
-    top = top.next;
-  }
-}
-
-console.log(findCell(ic31, 47));
