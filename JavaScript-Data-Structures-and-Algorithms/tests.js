9fb4d3ce1fa12f1c2f8b6f4de3559bf0080819c2
w3software-br <w3software.br@gmail.com>
2021-09-21 09:10:10 -0300

    #

--- JavaScript-Data-Structures-and-Algorithms/tests.js
+++ JavaScript-Data-Structures-and-Algorithms/tests.js
@@ -7,11 +7,35 @@ let arr = [
 ]
 
 function espiralArray(arr, size) {
-  let size = arr[0].length; 
   for(let i = 0; i < size; i++) {
-    console.log(arr[0][i])    
+    console.log(arr[0].pop(i))    
   }
+  console.log("\n");   
+  arr[0] = undefined
+  return arr.filter(element => element !== undefined);
 }
 
-espiralArray(arr, arr[0].length)
+function run(arr) {
+  arr = espiralArray(arr, arr[0].length)
+  let newArr = [];
+  if(arr.length > 0) {
+    let lines = arr.length;
+    let columns = arr[0].length;
+    let newArr = []
+    for(let col = columns - 1; col >= 0; col--) {
+      let a = []
+      for(let lin = 0; lin < lines; lin++) {
+        // console.log(lin, col)
+        a.push(arr[lin][col]);
+      }
+      newArr.push(a)
+    }
+    console.log(newArr)    
+  }
+  console.log();
+}
+
+run(arr)
+
+
 
