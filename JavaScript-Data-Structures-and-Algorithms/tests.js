51f41e5c7c522f7df5a4a121884c0dd61b842674
w3software-br <w3software.br@gmail.com>
2021-09-30 07:09:53 -0300

    #

--- JavaScript-Data-Structures-and-Algorithms/tests.js
+++ JavaScript-Data-Structures-and-Algorithms/tests.js
@@ -1,76 +1,37 @@
-'use strict'
 
-var board = 
+let board = 
 `%e%%%%%%%%%\n
 %...%.%...%\n
 %.%.%.%.%%%\n
 %.%.......%\n
 %.%%%%.%%.%\n
 %.%.....%.%\n
-%%%%%%%%%x%`;
+%%%%%%%%%x%`
 
 let rows = board.split("\n")
 
+rows = rows.filter((arr) => {
+  if(arr.length > 0) return arr.split("")
+})
+
 function generateColumnArr(arr) {
-  if(arr.length > 0) return arr.split("");
+  return arr.split("")
 }
 
-let mazeMatrix = rows.filter(generateColumnArr);
+let mazeMatrix = rows.map(generateColumnArr)
 
 function findChar(char, mazeMatrix) {
-  let row = mazeMatrix.length;
-  let column = mazeMatrix[0].length;
-
+  let row = mazeMatrix.length, 
+      column = mazeMatrix[0].length;
+  console.log(row, column)
   for(let i = 0; i < row; i++) {
     for(let j = 0; j < column; j++) {
-      if(mazeMatrix[i][j] == char) return [i, j];
+      console.log(i, j)
+      console.log(mazeMatrix[i][j])
+      console.log("\n");
+      // if(mazeMatrix[i][j] == char) return [i, j];
     }
   }
 }
 
-function printMatrix(matrix) {
-  let mazePrint = "",
-      row = matrix.length,
-      column = matrix[0].length;
-  
-  for(let i = 0; i < row; i++) {
-    for(let j = 0; j < column; j++) {
-      mazePrint += matrix[i][j];
-    }
-    mazePrint += "\n";
-  }
-  console.log(mazePrint);
-}
-
-function mazePathFinder(mazeMatrix) {
-  let row = mazeMatrix.length,
-      column = mazeMatrix[0].length,
-      starPos = findChar('e', mazeMatrix),
-      endPos = findChar('x', mazeMatrix);
-  
-  path(starPos, endPos);
-  function path(x, y) {
-    if (x > row - 1 || y > column - 1 || x < 0 || y < 0) {
-      return false;
-    }
-    // Found
-    if (x == endPos[0] && y == endPos[1]) {
-      return true;
-    }
-    if (mazeMatrix[x][y] == '%' || mazeMatrix[x][y] == '+') {
-      return false;
-    }
-    // Mark the current spot
-    mazeMatrix[x][y] = '+';
-    printMatrix(mazeMatrix);
-    if (path(x, y - 1) || path(x + 1, y) || path(x, y + 1) || path(x - 1, y)) {
-      return true;
-    }
-    mazeMatrix[x][y] = '.';
-    return false;
-  }
-}
-
-mazePathFinder(board);
-
-
+findChar('x', mazeMatrix)
