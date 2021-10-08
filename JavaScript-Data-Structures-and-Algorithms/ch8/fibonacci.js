23b8cc1a7adb1cefcd3231325ceabcb9714c5c94
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-10-08 08:44:01 -0300

    #

--- JavaScript-Data-Structures-and-Algorithms/ch8/fibonacci.js
+++ JavaScript-Data-Structures-and-Algorithms/ch8/fibonacci.js
@@ -1,3 +1,4 @@
+let test = 78;
 
 function fibonacci(n) {
   if(n <= 1) return n; // F1 = 1; F0 = 0
@@ -11,4 +12,27 @@ function fibonacci(n) {
   return sum;
 }
 
-console.log(fibonacci(68))
+// console.log(fibonacci(test))
+
+function getNthFibo(n) {
+  if(n <= 1) return n;
+  else {
+    return getNthFibo(n - 1) + getNthFibo(n - 2);
+  }
+}
+
+// console.log(getNthFibo(test))
+
+function getNthFiboBetter(n, lastlast, last) {
+  if(n == 0) {
+    console.log("n == 0 and lastlast = " + lastlast)
+    return lastlast;
+  }
+  else if(n == 1) {
+    console.log("n == 1 and last = " + last)
+    return last;
+  }
+  return getNthFiboBetter(n-1, last, last + lastlast);
+}
+// lastlast = last (fib0 = 0); last = last (fib0 = 0) + lastlast (fib1 = 1)
+console.log(getNthFiboBetter(test, 0, 1)); 
