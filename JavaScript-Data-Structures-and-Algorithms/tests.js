cb46e7b33c496ab81ad28d699f44cdd5fde33f77
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-10-14 08:37:31 -0300

    #

--- JavaScript-Data-Structures-and-Algorithms/tests.js
+++ JavaScript-Data-Structures-and-Algorithms/tests.js
@@ -1,19 +1,32 @@
 
-function pascalTriangle(cols, rows, mt=[1]) {
+let res = []
+function showPascalTriangle(cols, rows, mt=[1]) {
 	let show = [];
 	mt = mt.reverse();
-	if(mt.length == 1) console.log(mt); // mt = [1]
+	if(mt.length == 1) res.push(mt); // mt = [1]
 	show.push(mt[0])
   for(let i = 0; i < mt.length; i++) {
 		if(mt[i + 1]) show.push(mt[i] + mt[i+1]);
 		else show.push(mt[i])
 	}
 	mt = show.filter(el => el != 0);
-	console.log(mt) // response
+	res.push(mt) // response
 	cols++;
 	if(cols < rows) {
-		pascalTriangle(cols, rows, mt);
+		showPascalTriangle(cols, rows, mt);
 	}
 }
 
-pascalTriangle(0, 5)
+showPascalTriangle(0, 5);
+console.log(res);
+/*
+function pascalTriangle(row, col) {
+	if(col == 0) return 1;
+	else if(row == 0) return 0;
+	else {
+		return pascalTriangle(row - 1, col) + pascalTriangle(row - 1, col - 1)
+	}
+}
+
+console.log(pascalTriangle(5, 2))
+*/
