f6d909ede21df2e60817bb8326a63d70c2369d99
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-10-27 09:30:38 -0300

    #

--- JavaScript-Data-Structures-and-Algorithms/tests.js
+++ JavaScript-Data-Structures-and-Algorithms/tests.js
@@ -1,27 +1,32 @@
-let initArr = ['A', 'C', 'D'] 
-let count = 0;
+// my solution
+function permute(arr) {
+  let count = 0;
+  function main(arr) {  
+    let first = arr[0]
+    console.log(arr)
+    for (const el of arr) {
+      for(let i = 0; i < arr.length; i++) {
+        let temp = arr[i];
+        if(first == temp && arr[i + 1]) {
+          arr[i] = arr[i + 1];
+          arr[i + 1] = first;         
+        }   
+        
+        if(first == arr[arr.length - 1] && count < arr.length) {
+          main(arr);
+        }
+        else if(count < arr.length) {
+          console.log(arr);
+        }
+        if(count >= arr.length) return;
+        count++
 
-function permute(arr) {  
-  let first = arr[0]
-  console.log(arr)
-  for (const el of arr) {
-    for(let i = 0; i < arr.length; i++) {
-      let temp = arr[i];
-      if(first == temp && arr[i + 1]) {
-        arr[i] = arr[i + 1];
-        arr[i + 1] = first;         
-      }   
-      
-      if(first == arr[arr.length - 1] && count < initArr.length) {
-        permute(arr);
       }
-      else if(count < initArr.length) {
-        console.log(arr);
-      }
-      count++
-
     }
   }
-}
 
-permute(initArr);
+  main(arr)
+
+}
+let initialArr = ['A', 'C', 'D']
+permute(initialArr);
