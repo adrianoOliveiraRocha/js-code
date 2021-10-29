6e9bff77c169cabc780728f73855be10b95b2adc
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-10-29 08:19:58 -0300

    #

--- JavaScript-Data-Structures-and-Algorithms/tests.js
+++ JavaScript-Data-Structures-and-Algorithms/tests.js
@@ -1,5 +1,17 @@
 // WRITE A PROGRAM THAT RECURSIVELY DETERMINES IF A STRING IS A PALINDROME
-
-function main(word) {
-  console.log(word.join(','));
+// easy solution
+function itIsPalindrome(word) {
+  let x = word.split('')
+  let y = word.split('').reverse()
+  for(let i = 0; i < x.length; i++) {
+    if(x[0] != y[0]) {
+      return false;
+    }
+  }
+  return true;
 }
+
+console.log(itIsPalindrome('adriano'))
+console.log(itIsPalindrome('madam'))
+
+// recursive solution ?
