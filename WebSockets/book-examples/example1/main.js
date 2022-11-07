5274a76022277ed1bd968022a7b13ae67d186b58
adriano <adriano.qwe32@gmail.com>
2022-11-07 07:45:46 -0300

    #

--- WebSockets/book-examples/example1/main.js
+++ WebSockets/book-examples/example1/main.js
@@ -1,22 +1,55 @@
 if(window.WebSocket) {
   let buttonSend = document.getElementById('send-button');
-  buttonSend.onclick = function() {
-    console.log("Button clicked!");
-  }
+  let buttonStop = document.getElementById('stop-button');
+  let textView = document.getElementById("text-view");
+  let label = document.getElementById('status-label');
+
   //ws://localhost:3000?token=123456
   //wss://socketsbay.com/wss/v2/2/demo/
   const socket = new WebSocket("ws://localhost:3000?token=123456");
-  // open
+  
   socket.onopen = function(event) {
-    let label = document.getElementById('status-label');
     label.innerHTML = "Connection stabilished!";
   }
-  //message
+  
+  buttonSend.onclick = function() {
+    if(socket.readyState === WebSocket.OPEN) {
+      socket.send(textView.value);
+    } else {
+      console.log("No Connection!");
+    }
+  }
+  
+  buttonStop.onclick = function() {
+    if(socket.readyState === WebSocket.OPEN) {
+      socket.close();
+    }
+  }
+  
   socket.onmessage = function(event) {
     if(typeof event.data === 'string') {
-      let label = document.getElementById('status-label');
       label.innerHTML = event.data;
     }
+  }
+
+  socket.onclose = function(event) {
+    console.log('Connection closed!');
+
+    let code = event.code;
+    let reason = event.reason;
+    let wasClean = event.wasClean;
+
+    if(wasClean) {
+      label.innerHTML = "Connection closed normally"
+    } else {
+      label.innerHTML = "Connection closed with message " + reason 
+      + " (Code: " + code + ")";
+    }
+  }
+
+  socket.onerror = function(event) {
+    console.error("Oops!");
+    label.innerHTML = `Error: ${event}`;
   }
 
 } else {
