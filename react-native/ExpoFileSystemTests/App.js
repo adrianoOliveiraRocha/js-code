7f2cbbff61e00010f357daf6f0730c96b1690a3d
adriano <adriano.qwe32@gmail.com>
2023-01-24 09:45:24 -0300

    #

--- react-native/ExpoFileSystemTests/App.js
+++ react-native/ExpoFileSystemTests/App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as FileSystem  from 'expo-file-system';
 
 export default function App() {
+  
+  function saveFile() {
+    let fileUri = 'assets/files/text.txt';
+    let content = "File conent, File conent, File conent, File conent";
+    let options = {};
+    FileSystem.writeAsStringAsync(fileUri, content, options)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  } 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Save' onPress={saveFile} />
       <StatusBar style="auto" />
     </View>
   );
