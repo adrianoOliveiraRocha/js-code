3f21dda72d7a8721b421f4b46fbf4343a568a83e
adriano <adriano.qwe32@gmail.com>
2023-01-31 13:05:31 -0300

    #

---
+++ react-native/TestBare/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, View, Text, StyleSheet, SafeAreaView } from 'react-native';
+const RNFS = require('react-native-fs');
+
+function App() {
+
+  function basic() {
+    RNFS.readDir(RNFS.MainBundlePath)
+    .then(result => {
+      console.log("Got Result", result);
+    })
+  }
+
+  return (
+    <SafeAreaView style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>
+        Tests RNFS Bare
+      </Text>
+      <View style={styles.sectionDescription}>
+        <Button title='Test'/>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
