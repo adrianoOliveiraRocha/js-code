8da5a28594bec137fb5288caba123756d7d648fd
adriano <adriano.qwe32@gmail.com>
2023-02-01 10:50:03 -0300

    #

--- react-native/TestBare/App.js
+++ react-native/TestBare/App.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import { Button, View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Button, View, Text, StyleSheet, SafeAreaView, TouchableNativeFeedback, Alert } from 'react-native';
 import * as RNFS from 'react-native-fs';
 
 function App() {
 
   function basic() {
-    RNFS.readDir(RNFS.MainBundlePath)
+    //RNFS.DocumentDirectoryPath
+    RNFS.readDir(RNFS.DocumentDirectoryPath)
     .then(result => {
-      console.log("Got Result", result);
+      if(result[0].isFile()){
+        return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+      } else {
+        return false;
+      }      
     })
+    .then(statResult => {
+      if(statResult) {
+        return RNFS.readFile(statResult[1], 'utf8');
+      }      
+    })
+    .then(contents => {
+      alert(contents);
+    })
+    .catch(err => {
+      alert(err.message, err.code);
+    })
+    
+  }
+
+  function createFile() {
+    let path = RNFS.DocumentDirectoryPath + '/test.txt';
+    console.log(path);
+    RNFS.writeFile(path, "I am a productor. I dont need a job :/", 'utf8')
+    .then(success => {
+      alert('File Created');
+    })
+    .catch(err => {
+      console.warn(err);
+    })
+  }
+
+  function _delete() {
+    let path = RNFS.DocumentDirectoryPath + '/test.txt';
+    RNFS.unlink(path)
+    .then(() => {
+      alert('Deleted');
+    })
+    .catch(err => {
+      alert(err);
+    })
+
   }
 
   return (
@@ -17,7 +58,13 @@ function App() {
         Tests RNFS Bare
       </Text>
       <View style={styles.sectionDescription}>
-        <Button title='Test'/>
+        <Button title='Basic' onPress={basic}/>
+      </View>
+      <View style={styles.sectionDescription}>
+        <Button title='Create File' onPress={createFile}/>
+      </View>
+      <View style={styles.sectionDescription}>
+        <Button title='Delete' onPress={_delete}/>
       </View>
     </SafeAreaView>
   )
