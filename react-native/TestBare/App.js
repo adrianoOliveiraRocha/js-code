727b92ff72483b658310f47e89b031dc765bd321
adriano <adriano.qwe32@gmail.com>
2023-02-02 08:01:59 -0300

    #

--- react-native/TestBare/App.js
+++ react-native/TestBare/App.js
@@ -52,19 +52,48 @@ function App() {
 
   }
 
+  function readFile() {
+    let path = RNFS.DocumentDirectoryPath + '/test.txt';
+    RNFS.readFile(path, 'utf8')
+    .then(file => {
+      if(file) {
+        console.log(file);
+        alert(file);
+      }      
+    })
+    .catch(err => {
+      console.log(err);
+      alert('No file');
+    })
+  }
+
+  function readFromAssets() {
+    let path = RNFS.DocumentDirectoryPath + '/assets/test.txt';
+    RNFS.readFileAssets(path, 'utf8')
+    .then(file => {
+      alert(file);
+    })
+    .catch(err => {
+      alert(err);
+    })
+  }
+
   return (
     <SafeAreaView style={styles.sectionContainer}>
       <Text style={styles.sectionTitle}>
         Tests RNFS Bare
       </Text>
-      <View style={styles.sectionDescription}>
+      {/* <View style={styles.sectionDescription}>
         <Button title='Basic' onPress={basic}/>
-      </View>
+      </View> */}
       <View style={styles.sectionDescription}>
         <Button title='Create File' onPress={createFile}/>
       </View>
       <View style={styles.sectionDescription}>
         <Button title='Delete' onPress={_delete}/>
+      </View>
+      <View style={styles.sectionDescription}>
+        <Button title='Read' onPress={readFromAssets}/>
       </View>
     </SafeAreaView>
   )
